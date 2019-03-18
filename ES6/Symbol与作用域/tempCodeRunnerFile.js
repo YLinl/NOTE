1cd4738f8ab0c8b3a4de342d95415ea1fcb847c8
YLinl <670653022@qq.com>
2019-03-18 17:55:16 +0800

    '没什么改变'

---
+++ ES6/Symbol与作用域/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const a = require('./mod.js');
+console.log(a.foo);
---
+++ JavaScript/JavaScript高级程序设计V3/5引用类型/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ function add(num){
+   var len = num.length;
+   for(let i = 0; i<len; i++){
+     return sum +=num[i];
+   }
+ }
+ add(1,23,4);
